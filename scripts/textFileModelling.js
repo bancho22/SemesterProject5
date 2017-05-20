3fa3a02cba61d64c7cdc6305f73e4960505eb3c1
Alexander <gyurovalexander@gmail.com>
2017-05-20 17:59:28 +0300

    Fixed extra carriage

--- scripts/textFileModelling.js
+++ scripts/textFileModelling.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
-var ar1 = ['pepi','Sofia','kisaijlkacsjkl']
+var ar1 = ['pepi','Hulio','Sofia','kisaijlkacsjkl','Kefish']
 var ar2 = ['Stonheg','Copenhagen','Male']
-console.log( intersect_arrays(ar1,ar2) );
+//console.log( intersect_arrays(ar1,ar2) );
 
 function readLines(input, func) {
   var remaining = '';
@@ -13,7 +13,8 @@ function readLines(input, func) {
     while (index > -1) {
       var line = remaining.substring(last, index);
       last = index + 1;
-      
+    //   var izr = line.split(' ')
+    //   console.log( intersect_arrays(ar1,izr) )
       func(line);
       index = remaining.indexOf('\n', last);
       
@@ -29,11 +30,20 @@ function readLines(input, func) {
   });
 }
 function func(data) {
-    var dumi = data.split(' ');
+    //console.log('Purvo   ' + data)
+    var str = data.toString();
+    str = str.replace(/[\n\r]/g, '')
+    str =str.split(' ')
+    //console.log('to string' + str)
+   //var dumi = data.split(' ');
+    //console.log('Vtoro s split   ' + dumi)
     
 
+    // dumi = dumi.toString();
+    // dumi = dumi.replace(/[\n\r]/g, '')
     
- console.log( intersect_arrays(ar1,dumi) );
+    // console.log(dumi)
+ console.log( intersect_arrays(ar1,str) );
 }
 
 function intersect_arrays(a, b) {
