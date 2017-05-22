0d574395cdf05051411473ab3f25a5b9b787b007
Bancho Petrov <bancho@vaavud.com>
2017-05-22 17:25:10 +0200

    insertMany() instead of insertOne()

--- scripts/bookScanner/bookScanner.js
+++ scripts/bookScanner/bookScanner.js
@@ -32,8 +32,6 @@ const bookTitle = (linesInBook) => {
     }, '').trim()
 }
 
-let insertedTotal = 0
-let errorsTotal = 0
 
 dir.files('scripts/bookScanner/books', (err, bookFiles) => {
     console.log('bookFiles num', bookFiles.length)
@@ -41,40 +39,58 @@ dir.files('scripts/bookScanner/books', (err, bookFiles) => {
     cityGetter(citiesByNameId => {
         console.log('cities num', citiesByNameId.length)
 
-        bookFiles.map(fileName => {
+        let dbEntries = bookFiles.map(fileName => {
             let book = fs.readFileSync(fileName).toString()
             let linesInBook = book.split('\n')
             let wordsInBook = book.replace(/[\n\r]/g, ' ').split(' ')
 
-            let dbEntry = {
+	        console.log(fileName, 'in processing')
+
+            return {
                 author: bookAuthor(linesInBook),
                 title: bookTitle(linesInBook),
                 citiesMentioned: matchingCitiesInBook(citiesByNameId, book)
             }
 
-            mongo.get().collection('books').insertOne(dbEntry, (err, r) => {
-                if(err){
-                    errorsTotal++
-                }
-                else{
-                    insertedTotal += r.insertedCount
-                    console.log('book', insertedTotal, 'inserted:', dbEntry.title)
-                }
+            // mongo.get().collection('books').insertOne(dbEntry, (err, r) => {
+            //     if(err){
+            //         errorsTotal++
+            //     }
+            //     else{
+            //         insertedTotal += r.insertedCount
+            //         console.log('book', insertedTotal, 'inserted:', dbEntry.title)
+            //     }
 
-                if(errorsTotal + insertedTotal === bookFiles.length){
-                    console.log('insertedTotal:', insertedTotal, '\nerrorsTotal', errorsTotal)
-                    console.log('closing mongo connection...')
-                    mongo.close()
-                        .then(() => {
-                            console.log('Bye...')
-                            process.exit(0)
-                        })
-                        .catch(err => {
-                            console.log('Could not close connection', err.toString())
-                            process.exit(1)
-                        })
-                }
-            })
+            //     if(errorsTotal + insertedTotal === bookFiles.length){
+            //         console.log('insertedTotal:', insertedTotal, '\nerrorsTotal', errorsTotal)
+            //         console.log('closing mongo connection...')
+            //         mongo.close()
+            //             .then(() => {
+            //                 console.log('Bye...')
+            //                 process.exit(0)
+            //             })
+            //             .catch(err => {
+            //                 console.log('Could not close connection', err.toString())
+            //                 process.exit(1)
+            //             })
+            //     }
+            // })
         })
+
+	    // dbEntries.forEach(e => {console.log(e.title, 'by', e.author)})
+
+        console.log('dbEntries.length before filter', dbEntries.length)
+        dbEntries = dbEntries.filter(({author}, {title}) => {!(!author && !title)}) // to this day cannot understand why (author && title) won't work'
+        console.log('dbEntries.length after filter', dbEntries.length, '\n')
+
+        mongo.get().collection('books').insertMany(dbEntries, (err, r) => {
+            if(err){
+                console.log(err)
+                process.exit(1)
+            }
+            console.log(r)
+            process.exit(0)
+        })
+
     })
 })
